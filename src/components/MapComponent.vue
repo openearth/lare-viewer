690d5f8a4c0bfa47c7fd1b016c2802e99868a424
luisrodriguezgalvez <77997772+luisrodriguezgalvez@users.noreply.github.com>
2026-01-27 13:55:18 +0100

    initial commit, map not working

---
+++ src/components/MapComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="map-wrapper">
+    <mapbox-map
+      :access-token="accessToken"
+      :center="[4.7, 52.2]"
+      map-style="mapbox://styles/mapbox/light-v11"
+      :zoom="7"
+    />
+  </div>
+</template>
+
+<script setup>
+  import { MapboxMap } from '@studiometa/vue-mapbox-gl'
+
+  const accessToken = import.meta.env.VITE_MAPBOX_TOKEN
+</script>
+<style>
+.map-wrapper,
+.map-wrapper .mapboxgl-map {
+  width: 100%;
+  height: 100%;
+}
+</style>
